892fd34025c01d347bc27253057f9514a7d2b2b2
Raunak Sen <144312743+rksinthecity@users.noreply.github.com>
2025-07-31 08:46:15 +0530

    Update MirrorOfErised.tsx

--- frontend/src/pages/MirrorOfErised.tsx
+++ frontend/src/pages/MirrorOfErised.tsx
@@ -59,7 +59,7 @@ const MirrorOfErised: React.FC = () => {
   };
 
   const fetchDailyQuote = async () => {
-    const data = await fetchQuote('http://localhost:5000/api/erised/daily');
+    const data = await fetchQuote('https://wizedia-backend-2.onrender.com');
     if (data) {
       setQuoteState({
         quote: data.quote,
